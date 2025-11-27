2e0b7f15eb0cb50c4f09e605b53aaa6a96a079d1
Bartodziej <bartixboss@gmail.com>
2025-11-27 18:01:05 +0100

    Configuration window

---
+++ src/main/java/com/example/musica/Musica.java
@@ -0,0 +1,16 @@
+package com.example.musica;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Musica extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
